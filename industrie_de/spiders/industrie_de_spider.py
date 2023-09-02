634a28047c9691880ea10887ac15355a46a4d1a9
Alina Dovhalevska <alina_dovhalevska@alinas-air.localdomain>
2023-09-03 01:56:59 +0200

    format-sorting according to icon class, added comments to the code

--- industrie_de/spiders/industrie_de_spider.py
+++ industrie_de/spiders/industrie_de_spider.py
@@ -7,37 +7,49 @@ class IndustrieDeSpiderSpider(scrapy.Spider):
 
 
     def parse(self, response):
-        company_eitrag = response.css('.infoservice-result-row')
-        for link in company_eitrag.css('a::attr(href)').getall():
-              if link:
-                yield scrapy.Request(response.urljoin(link), callback=self.parse_company_page)
+      #get all company entries from the list
+      company_eitrag = response.css('.infoservice-result-row')
+      #extract the links from the company entries and tell scrapy to loop through it
+      for link in company_eitrag.css('a::attr(href)').getall():
+        #doing so only if link exists
+        if link:
+          yield scrapy.Request(response.urljoin(link), callback=self.parse_company_page)
 
 
     def parse_company_page(self, response):
-        company_name = response.css('.h2 ::text').get()
-        if not company_name:
-          company_name = ["N/A"]
+      #get the company name
+      company_name = response.css('.h2 ::text').get()
+      if not company_name:
+        company_name = ["N/A"]
 
-        try:
-          contact_info = response.css('.textwidget dd.info-contact-data::text').getall()
-          #contact_info = response.xpath('//div[@class="textwidget"]/dl/dd/text()').getall()
-          if contact_info:
-            website = contact_info[0]
-            email = contact_info[1]
-            phone = contact_info[2]
-            fax = contact_info[3]
-            address = ",".join(contact_info[4:8])
-          else:
-            website = email = phone = fax = address = "N/A"
-        except Exception as e:
-              #Handle exceptions like failed requests or unexpected HTML structure
-              print("No information found")
+      #set values on NA in case info is missing
+      website = email = phone = fax = address = "N/A"
 
-        yield {
-          'Company Name': company_name,
-          'Website': website,
-          'Email': email,
-          'Phone': phone,
-          'Fax': fax,
-          'Address': address,
-        }
+      # contact_info = response.css('.textwidget dd.info-contact-data::text').getall()
+      # contact_info = response.xpath('//div[@class="textwidget"]/dl/dd/text()').getall()
+
+      try:
+        #getting info from Daten&Kontakte and format-sorting according to icon class in case some entries are empty in Daten&Kontakte
+        if response.css('.info-icon i.fa-globe'):
+          website = response.css('dd.info-contact-data::text').extract_first()
+        if response.css('.info-icon i.fa-envelope'):
+          email = response.css('dd.info-contact-data::text').extract()[1]
+        if response.css('.info-icon i.fa-phone'):
+          phone = response.css('dd.info-contact-data::text').extract()[2]
+        if response.css('.info-icon i.fa-fax'):
+          fax = response.css('dd.info-contact-data::text').extract()[3]
+        if response.css('.info-icon i.fa-map-pin'):
+          address_parts = response.css('dd.info-contact-data::text').extract()[4:8] #could be nicer, but it is what it is
+          address = ", ".join(address_parts)
+      except Exception as e:
+        #double protection against cases where there are icons somewhere, but no infos etc
+        print("No information found")
+
+      yield {
+        'Company Name': company_name,
+        'Website': website,
+        'Email': email,
+        'Phone': phone,
+        'Fax': fax,
+        'Address': address,
+      }
